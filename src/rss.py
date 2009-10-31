a5d1ef122e5ae5d413d84cb9ac13cbe4bd7805e8
Kurt McKee <contactme@kurtmckee.org>
2009-10-31 14:29:33 -0500

    fix odd license text ("that license"?)

--- src/rss.py
+++ src/rss.py
@@ -5,20 +5,20 @@
 # Copyright 2009 Adam Sampson <ats@offog.org>
 # Copyright 2009 Kurt McKee <contactme@kurtmckee.org>
 #
-# rawdog_rss is free software; you can redistribute and/or modify it
-# under the terms of that license as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# rawdog_rss is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with rawdog_rss; see the file COPYING. If not, write to the Free
-# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA, or see http://www.gnu.org/.
+# rawdog_rss is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# rawdog_rss is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with rawdog_rss; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You can also visit http://www.gnu.org/ .
 #
 # ---
 #
